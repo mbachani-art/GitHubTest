2fbb78dc630ecdae033a2604a89236fe0f8079a9
Megha Bachani <meghabachani@gmail.com>
2022-04-12 19:59:27 -0400

    changed msg

--- TT/WpfApp1/WpfApp1/MainWindow.xaml.vb
+++ TT/WpfApp1/WpfApp1/MainWindow.xaml.vb
@@ -2,7 +2,7 @@
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
         Dim name As String
         name = txtName.Text
-        MessageBox.Show("Hi " & name)
+        MessageBox.Show("Hello world " & name)
     End Sub
 
 
